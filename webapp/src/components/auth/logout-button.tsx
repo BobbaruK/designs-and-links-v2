ddbf7337bb02ef61e31cd6684efc9a37998c2a33
Bobbaru <bobbaru_krueger@yahoo.com>
2024-09-27 23:34:46 +0300

    better understanding of NEXTAUTH_URL && update prisma related packages

--- webapp/src/components/auth/logout-button.tsx
+++ webapp/src/components/auth/logout-button.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { logout } from "@/actions/logout";
 import { ReactNode } from "react";
-
-/**
- * signOut sends you to localhost:3000. this
- * project has ports mapped to 3001 and 5433
- */
-// import { signOut } from "next-auth/react";
+import { signOut } from "next-auth/react";
 
 interface Props {
   children?: ReactNode;
 }
 
 export const LogoutButton = ({ children }: Props) => {
-  const onClick = () => logout();
-
   return (
-    <span onClick={() => onClick()} className="cursor-pointer">
+    <span onClick={() => signOut()} className="cursor-pointer">
       {children}
     </span>
   );
